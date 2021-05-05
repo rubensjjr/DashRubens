791d724afb8ef0fdf113428978d561a225cb32c7
Rubens Junior <rubens.junior@outlook.com.br>
2021-05-05 10:05:41 -0300

    Configuração de segurança

--- backend/src/main/java/com/rubens/dashboard/DashRubensApplication.java
+++ backend/src/main/java/com/rubens/dashboard/DashRubensApplication.java
@@ -1,4 +1,4 @@
-package com.rubens.dashboard.demo;
+package com.rubens.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
