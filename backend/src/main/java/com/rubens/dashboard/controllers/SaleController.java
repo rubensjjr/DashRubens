315ae12ff7e6aebbfced8de7bcbc3570ade143ef
Rubens Junior <rubens.junior@outlook.com.br>
2021-05-05 16:42:09 -0300

    Buscas agrupadas

--- backend/src/main/java/com/rubens/dashboard/controllers/SaleController.java
+++ backend/src/main/java/com/rubens/dashboard/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.rubens.dashboard.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rubens.dashboard.dto.SaleDTO;
+import com.rubens.dashboard.dto.SaleSucessDTO;
+import com.rubens.dashboard.dto.SaleSumDTO;
 import com.rubens.dashboard.service.SaleService;
 
 @RestController
@@ -21,6 +25,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value="/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupBySeller() {
+		List<SaleSumDTO> list = service.amountGroupBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value="/success-by-seller")
+	public ResponseEntity<List<SaleSucessDTO>> sucessGroupBySeller() {
+		List<SaleSucessDTO> list = service.sucessGroupBySeller();
 		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/rubens/dashboard/service/SaleService.java
+++ backend/src/main/java/com/rubens/dashboard/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.rubens.dashboard.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rubens.dashboard.dto.SaleDTO;
+import com.rubens.dashboard.dto.SaleSucessDTO;
+import com.rubens.dashboard.dto.SaleSumDTO;
 import com.rubens.dashboard.entities.Sale;
 import com.rubens.dashboard.repositories.SaleRepository;
 import com.rubens.dashboard.repositories.SellerRepository;
@@ -25,6 +29,16 @@ public class SaleService {
 		Page<Sale> result =repository.findAll(pagable);
 		return 	 result.map(x -> new SaleDTO(x));
 
+	}
+	
+	@Transactional(readOnly = true)
+	public List <SaleSumDTO> amountGroupBySeller(){
+		return repository.amountGroupBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List <SaleSucessDTO> sucessGroupBySeller(){
+		return repository.sucessGroupBySeller();
 	}
 
 }
